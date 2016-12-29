673569c2a91803a3e3b1bce4cb1fb35c77ce034d
longxuanho <longxuanho@gmail.com>
2016-12-29 11:52:39 +0700

    adding nhap-lieu-list-all page

--- src/app/+nhap-lieu/nhap-lieu-list-all/nhap-lieu-list-all.component.ts
+++ src/app/+nhap-lieu/nhap-lieu-list-all/nhap-lieu-list-all.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SuaChuaService } from '../../core/shared/sua-chua.service';
+import { SuaChua, TrangThaiSuaChua } from '../../core/shared/sua-chua.model';
+import { Subscription } from 'rxjs/Subscription';
+import { ToastrService } from 'toastr-ng2';
 
 @Component({
   selector: 'sk-nhap-lieu-list-all',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NhapLieuListAllComponent implements OnInit {
 
-  constructor() { }
+  subscriptions: { suachuasCurrent?: Subscription, suachuasDone?: Subscription } = {}
+  suachuasCurrent: SuaChua[] = [];
+  suachuasDone: SuaChua[] = [];
+  trangThaiSuaChua: any;
+
+  constructor(
+    private suaChuaService: SuaChuaService,
+    private toastrService: ToastrService
+  ) {
+    this.trangThaiSuaChua = TrangThaiSuaChua;
+  }
 
   ngOnInit() {
+    this.subscriptions.suachuasCurrent = this.suaChuaService.getSuaChuasCurrent().subscribe(
+      snapshots => {
+        this.suachuasCurrent = <SuaChua[]>snapshots;
+      }, (error: Error) => this.toastrService.error(`Không thể truy vấn dữ liệu. ${error.message}`, 'Opps!')
+    )
+    this.subscriptions.suachuasDone = this.suaChuaService.getSuaChuasDone().subscribe(
+      snapshots => {
+        this.suachuasDone = <SuaChua[]>snapshots;
+      }, (error: Error) => this.toastrService.error(`Không thể truy vấn dữ liệu. ${error.message}`, 'Opps!')
+    )
   }
+
+  ngOnDestroy() {
+    this.subscriptions.suachuasCurrent.unsubscribe();
+    this.subscriptions.suachuasDone.unsubscribe();
+  }
+  
 
 }
