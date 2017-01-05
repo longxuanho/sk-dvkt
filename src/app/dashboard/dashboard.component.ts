f0148ffff33e827fc0161fd16cbfb5c4cd953fae
Long Hồ <Long H>
2017-01-05 22:45:21 +0700

    prototyping dashboard - ok

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'sk-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    $('body').addClass("bg-dashboard");
+  }
+
+  ngOnDestroy() {
+    $('body').removeClass("bg-dashboard")
   }
 
 }
