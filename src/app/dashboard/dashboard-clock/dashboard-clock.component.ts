5a486be23392cb41659f9857fdae4df3d887e42a
longxuanho <longxuanho@gmail.com>
2017-01-12 15:51:25 +0700

    adding new clock

--- src/app/dashboard/dashboard-clock/dashboard-clock.component.ts
+++ src/app/dashboard/dashboard-clock/dashboard-clock.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+declare var moment: any;
 
 @Component({
   selector: 'sk-dashboard-clock',
   templateUrl: './dashboard-clock.component.html',
   styleUrls: ['./dashboard-clock.component.scss']
 })
-export class DashboardClockComponent implements OnInit {
+export class DashboardClockComponent implements OnInit, OnDestroy {
+
+  currentTime = {
+    hour: '0',
+    minute: '00',
+    suffix: 'SA',
+    display: ''  
+  };
+  showCollon: boolean = false;
+  timer: NodeJS.Timer;
 
   constructor() { }
 
   ngOnInit() {
+    this.timer = setInterval(() => {
+      let now = moment();
+      this.currentTime.hour = now.format('h');
+      this.currentTime.minute = now.format('mm');
+      this.currentTime.suffix = now.format('A');
+      this.currentTime.display = now.format('dddd, [ngày] DD [tháng] MM [năm] YYYY');
+      this.showCollon = !this.showCollon;
+    }, 1500);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timer);
   }
 
 }
